b0e0d3af488cb577ddff7c5badf531b18ca4d69c
ritchiec5 <2609774C@student.gla.ac.uk>
2022-05-29 00:23:00 +0800

    Actual FL Module: Reorg, Documenting, Refactoring

---
+++ ML Module/Actual FL_module/Client/Client.py
@@ -0,0 +1,92 @@
+import os 
+import tensorflow
+from tensorflow.keras.callbacks import ModelCheckpoint
+import tensorflow.keras.callbacks as callbacks
+import numpy
+from flask import Flask, request, jsonify
+import requests
+import threading
+from libs.chess_utils import * 
+
+app = Flask(__name__)
+
+""" 
+request_global_model()
+    Request global model from the server using python Request
+"""
+def request_global_model():
+    print('Requesting Global Model from Server/n')
+    res = requests.get('http://localhost:5000/server/send_global_weights',
+                       json="Requesting Global model Data")
+    print('received Global Model from Server/n')
+    with open("C:/Users/Ritchie Chan/Desktop/3004/Federated_Learning_ChessAI/ML Module/Actual FL_module/Client/global_model", "wb") as f:
+        f.write(res.content)
+    f.close()
+    print('Global Model saved')
+
+"""
+client_model_training()
+    1. Train the Global Model from the Server using client dataset
+    2. Clients saves the weights in a h5 file
+"""
+def client_model_training(dataset):
+    x_train, y_train = dataset 
+    x_train.transpose()
+    checkpoint_filepath = '/tmp/checkpoint/'
+    model_checkpointing_callback = ModelCheckpoint(
+        filepath=checkpoint_filepath,
+        save_best_only=True,
+    )
+
+    print("Initiated Model training/n")
+    chess_model = tensorflow.keras.models.load_model("C:/Users/Ritchie Chan/Desktop/3004/Federated_Learning_ChessAI/ML Module/Actual FL_module/Client/global_model")
+    chess_model.fit(x_train, y_train,
+            batch_size=2048,
+            epochs=5,
+            verbose=1,
+            validation_split=0.1,
+            callbacks=[callbacks.ReduceLROnPlateau(monitor='loss', patience=10),
+                        callbacks.EarlyStopping(monitor='loss', patience=15, min_delta=1e-4), model_checkpointing_callback])
+    chess_model.save_weights("C:/Users/Ritchie Chan/Desktop/3004/Federated_Learning_ChessAI/ML Module/Actual FL_module/Client/client_weights", save_format="h5")
+    print("Completed Model training/n")
+
+"""
+send_client_weights()
+    Send client weights to the server 
+"""
+def send_client_weights():
+    file = open("C:/Users/Ritchie Chan/Desktop/3004/Federated_Learning_ChessAI/ML Module/Actual FL_module/Client/client_weights", "rb")
+    params = {'client': '1', 'datasize':'100'}
+    res = requests.post('http://localhost:5000/server/receive_client_weights', file, params=params)
+    print('Response from server: ', res.text)
+
+"""
+receive_global_weights()
+    1. receive global weights from server
+    2. Save global weights in a file
+"""
+@app.route('/client/receive_global_weights', methods=['POST'])
+def receive_global_weights():
+    print("received Global weights")
+    if request.method == 'POST':
+        filename = "C:/Users/Ritchie Chan/Desktop/3004/Federated_Learning_ChessAI/ML Module/Actual FL_module/Client/global_weights"
+        with open(filename, "wb") as f:
+            f.write(request.data)
+        f.close()
+        return jsonify("received updated server weights")
+
+"""
+Initialize():
+    Temporary function to Initialize client to train and send weights
+"""
+def intialize():
+    request_global_model()
+    dataset = create_client_dataset()
+    client_model_training(dataset)
+    send_client_weights()
+
+if __name__ == '__main__':
+    # Threading is utilized to allow the Flask to run without waiting for training to be completed
+    intialize_thread = threading.Thread(target=intialize)  
+    intialize_thread.start() 
+    app.run(port=5001, debug=False)
---
+++ ML Module/Actual FL_module/Client/libs/chess_utils.py
@@ -0,0 +1,106 @@
+import chess
+import chess.engine
+import random
+import numpy
+
+"""
+Temporary code for generating Chess dataset - will be deleted when we get the data directly from the clients
+- Code generates chess boards
+- Chessboards are converted to 14 X 8 X 8
+"""
+
+def create_client_dataset():
+    x_dataset = []
+    y_dataset = []
+
+    print("Creating Dataset")
+    for i in range(10):
+        print("Dataset ", i, ": created")
+        board = random_board()
+        eval = stockfish(board, 1)
+        print("Stockfish: ", eval)
+        data_board = split_dims(board)
+
+        if eval is None:
+            pass
+        else:
+            x_dataset.append(data_board)
+            y_dataset.append(eval)
+
+    x_dataset = numpy.asarray(x_dataset)
+    y_dataset = numpy.asarray(y_dataset)
+    y_dataset = numpy.asarray(
+        y_dataset / abs(y_dataset).max() / 2 + 0.5, dtype=numpy.float32)
+    print(y_dataset)
+
+    return x_dataset, y_dataset
+
+# this function will create our x (board)
+def random_board(max_depth=200):
+  board = chess.Board()
+  depth = random.randrange(0, max_depth)
+
+  for _ in range(depth):
+    all_moves = list(board.legal_moves)
+    random_move = random.choice(all_moves)
+    board.push(random_move)
+    if board.is_game_over():
+      break
+
+  return board
+
+
+# this function will create our f(x) (score)
+def stockfish(board, depth):
+  with chess.engine.SimpleEngine.popen_uci(
+          r'C:\Users\Ritchie Chan\Desktop\3004\Federated_Learning_ChessAI\ML Module\Test module_Chess_AI\stockfish_15_win_x64_avx2\stockfish_15_win_x64_avx2\stockfish_15_x64_avx2.exe') as sf:
+    result = sf.analyse(board, chess.engine.Limit(depth=depth))
+    score = result['score'].white().score()
+    return score
+
+
+squares_index = {
+    'a': 0,
+    'b': 1,
+    'c': 2,
+    'd': 3,
+    'e': 4,
+    'f': 5,
+    'g': 6,
+    'h': 7
+}
+
+
+# example: h3 -> 17
+def square_to_index(square):
+  letter = chess.square_name(square)
+  return 8 - int(letter[1]), squares_index[letter[0]]
+
+
+def split_dims(board):
+  # this is the 3d matrix
+  board3d = numpy.zeros((14, 8, 8), dtype=numpy.int8)
+
+  # here we add the pieces's view on the matrix
+  for piece in chess.PIECE_TYPES:
+    for square in board.pieces(piece, chess.WHITE):
+      idx = numpy.unravel_index(square, (8, 8))
+      board3d[piece - 1][7 - idx[0]][idx[1]] = 1
+    for square in board.pieces(piece, chess.BLACK):
+      idx = numpy.unravel_index(square, (8, 8))
+      board3d[piece + 5][7 - idx[0]][idx[1]] = 1
+
+  # add attacks and valid moves too
+  # so the network knows what is being attacked
+  aux = board.turn
+  board.turn = chess.WHITE
+  for move in board.legal_moves:
+      i, j = square_to_index(move.to_square)
+      board3d[12][i][j] = 1
+  board.turn = chess.BLACK
+  for move in board.legal_moves:
+      i, j = square_to_index(move.to_square)
+      board3d[13][i][j] = 1
+  board.turn = aux
+
+  return board3d
